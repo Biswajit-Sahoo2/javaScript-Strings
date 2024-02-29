92344db6556b1aa4a54f21702b4bdf13859244f9
Biswajit-Sahoo2 <biswajitnov11@gmail.com>
2024-02-29 21:41:39 +0530

    Initial commit

---
+++ print_duplicate_characters.js
@@ -0,0 +1,16 @@
+const mapOfString = str => {
+    const map ={};
+    str.split("").forEach(i => map[i] = map[i] ? map[i] + 1: 1);
+    return map;
+   } 
+   const duplicateString = str => {
+    const arr =[];
+    const obj = mapOfString(str)
+    for(let j in obj){
+        if(obj[j] > 1){
+            arr.push(j)
+        }
+    }
+    return arr.join("")
+   }
+   console.log(duplicateString("javaScript"));
