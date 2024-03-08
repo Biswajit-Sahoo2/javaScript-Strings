f6274d0f4029bce212b1338a2aa6937a14fff0b0
Biswajit-Sahoo2 <biswajitnov11@gmail.com>
2024-03-08 17:59:23 +0530

    Added comment

--- count_vowels.js
+++ count_vowels.js
@@ -1,10 +1,12 @@
 //Q- Count the numbers of vowels present in a String.
 
 let str = "Biswajit sahoo";
+//Convert the String to lower case 
 let lowerStr = str.toLowerCase();
 let count = 0;
 
 for (let i = 0; i < lowerStr.length; i++) {
+    //Check every characters in the string, If a character is vowel, then increase the count
     if (lowerStr[i] === 'a' || lowerStr[i] === 'e' || lowerStr[i] === 'i' || lowerStr[i] === 'o' || lowerStr[i] === 'u' ) {
         count++;
     }
